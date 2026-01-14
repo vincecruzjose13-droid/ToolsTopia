6d919be076616270a41024e1c344d826551f4dd8
vincecruzjose13-droid <vincecruzjose13@gmail.com>
2026-01-14 22:10:16 +0800

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,31 @@
-<script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-database-compat.js"></script>
+// Firebase CDN loaders
+const firebaseAppScript = document.createElement("script");
+firebaseAppScript.src = "https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js";
+document.head.appendChild(firebaseAppScript);
 
-<script>
-const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  databaseURL: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+const firebaseAuthScript = document.createElement("script");
+firebaseAuthScript.src = "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth-compat.js";
+document.head.appendChild(firebaseAuthScript);
+
+const firebaseDbScript = document.createElement("script");
+firebaseDbScript.src = "https://www.gstatic.com/firebasejs/9.22.1/firebase-database-compat.js";
+document.head.appendChild(firebaseDbScript);
+
+firebaseDbScript.onload = () => {
+
+  const firebaseConfig = {
+    apiKey: "PASTE_YOUR_KEY",
+    authDomain: "PASTE_YOUR_DOMAIN",
+    databaseURL: "PASTE_YOUR_DB",
+    projectId: "PASTE_YOUR_PROJECT",
+    storageBucket: "PASTE_YOUR_BUCKET",
+    messagingSenderId: "PASTE_YOUR_SENDER",
+    appId: "PASTE_YOUR_APPID"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  window.auth = firebase.auth();
+  window.db = firebase.database();
+
+  console.log("✅ Firebase connected successfully");
 };
-
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
-</script>
